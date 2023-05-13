9f2e008ea095dd9e11cd2b84419b1e4882158042
Diego Martínez Sánchez <diegomartinez@tuhabi.mx>
2023-05-13 11:11:01 -0600

    Update websocket API
    Now the websocket API has their own repository

--- run_app.py
+++ run_app.py
@@ -1,10 +1,20 @@
 try:
     from app import app
+    # from gevent.pywsgi import WSGIServer
 except ImportError as e_imp:
     print(f"The following import ERROR occurred in {__file__}: {e_imp}")
 
-if __name__ == "__main__":
+if __name__== "__main__":
     try:
-        app.run()
-    except Exception as err:
-        print(f"The following ERROR occurred in {__file__}: {err}")
+        # -----------------Dev mode-----------------
+        # run app and print all print from the app
+        app.run(host="127.0.0.1", port=5001, debug=False)
+        # debug= True for apply changes made into the files without restarting the flask server
+
+        # -----------------Prod mode----------------
+        # appServer= WSGIServer(("127.0.0.1", 5000), app)
+        # appServer.serve_forever()
+    except Exception as e_imp:
+        print(f"The following ERROR occurred in {__file__}: {e_imp}")
+    finally:
+        print("Finishing program")
