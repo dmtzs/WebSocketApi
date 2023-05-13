cfddd0ab12ba96ffbfaf9199ad15081eda888853
Diego Martínez Sánchez <diegomartinez@tuhabi.mx>
2023-05-13 11:49:29 -0600

    Update app/__init__.py
    Added a condition in which the flask API will not order the JSON keys

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,6 @@ except ImportError as e_imp:
     print(f"The following import ERROR occurred in {__file__}: {e_imp}")
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
 from app import routes, error_handlers
